5851e91ac2d7202b5a24073774ff14ad34a04df4
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-02-12 07:01:30 +0100

    remove footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Analytics } from "@vercel/analytics/react";
 import cx from "classnames";
 import { sfPro, inter } from "./fonts";
 import Nav from "@/components/layout/nav";
-import Footer from "@/components/layout/footer";
 import { Suspense } from "react";
 
 export const metadata = {
@@ -22,14 +21,12 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={cx(sfPro.variable, inter.variable)}>
-        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
         <Suspense fallback="...">
           <Nav />
         </Suspense>
-        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+        <main className="flex min-h-screen w-full flex-col items-center justify-center py-8">
           {children}
         </main>
-        <Footer />
         <Analytics />
       </body>
     </html>
