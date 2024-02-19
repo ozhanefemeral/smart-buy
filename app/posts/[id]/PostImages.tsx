cdde420616bde42d42ee435a44e0b88bea06f796
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-02-19 20:53:10 +0300

    feat: `PostImages`
    - add changable images to `posts/[id]`

---
+++ app/posts/[id]/PostImages.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import placeholder from "@/public/placeholder.svg";
+import { useState } from "react";
+import { Post } from "@prisma/client";
+
+type PostImagesProps = {
+  post: Post;
+};
+
+const S3BaseUrl = process.env.NEXT_PUBLIC_S3_BASE_URL;
+
+export const PostImages: React.FC<PostImagesProps> = ({ post }) => {
+  const { images, title } = post;
+  const [currentImage, setCurrentImage] = useState(images[0]);
+
+  return (
+    <div className="flex max-h-[500px] flex-col border-b pb-4 lg:flex-row lg:space-x-4">
+      <div className="flex justify-center lg:w-4/5">
+        <div className="h-full overflow-hidden rounded-lg">
+          <Image
+            src={S3BaseUrl + "/" + currentImage || placeholder}
+            alt={title}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "auto", height: "100%" }}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col space-y-2 overflow-y-auto pr-4 lg:w-1/5">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`shrink-0 cursor-pointer overflow-hidden rounded-lg ${
+              currentImage === image ? "border-4 border-blue-500" : ""
+            }`}
+            onClick={() => setCurrentImage(image)}
+          >
+            <Image
+              src={S3BaseUrl + "/" + image || placeholder}
+              alt={title}
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -7,7 +7,7 @@ import { getPostById } from "@/lib/queries/post";
 import { Post } from "@prisma/client";
 import { PostOwnerInfo } from "@/components/post-owner/PostOwnerInfo/PostOwnerInfo";
 import SharePostCard from "./SharePost";
-import { S3BaseUrl } from "@/lib/s3";
+import { PostImages } from "./PostImages";
 
 type Params = {
   id: Post["id"];
@@ -22,21 +22,8 @@ export default async function PostPage({ params }: { params: Params }) {
     <Suspense>
       <div className="flex flex-col items-stretch space-y-4 lg:flex-row lg:items-start lg:space-x-4 lg:space-y-0">
         <div className="flex flex-col space-y-4 xl:w-2/3">
-          <div className=" rounded-lg border p-4">
-            <div className="flex max-h-[500px] flex-col border-b pb-4 lg:flex-row lg:space-x-4">
-              <div className="overflow-hidden rounded-lg lg:w-4/5">
-                <Image
-                  src={`${S3BaseUrl}/${post.images[0]}` || placeholder}
-                  alt={post.title}
-                  // below is the fix for having a 100% width image
-                  // discussed on: https://github.com/vercel/next.js/discussions/18474#discussioncomment-5501724
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{ width: "100%", height: "auto" }}
-                />
-              </div>
-            </div>
+          <div className="rounded-lg border p-4">
+            <PostImages post={post} />
             <div className="flex flex-col space-y-2 pt-4">
               <h1 className="text-2xl font-semibold">{post.title}</h1>
               <p className="max-h-96 overflow-y-auto whitespace-pre-wrap">
