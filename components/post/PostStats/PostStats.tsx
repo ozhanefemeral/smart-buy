88f2906df4745756e8c2b3f9d264c4fb80192b80
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-02-28 10:19:47 +0300

    refactor: replace text with `CountingNumbers`

--- components/post/PostStats/PostStats.tsx
+++ components/post/PostStats/PostStats.tsx
@@ -1,3 +1,4 @@
+import CountingNumbers from "@/components/shared/counting-numbers";
 import { Card, CardContent } from "@/components/ui/card";
 import { getPostStats } from "@/lib/aws";
 
@@ -19,13 +20,17 @@ export async function PostStats({ postId }: PostStatsProps) {
         <div className="flex justify-between">
           <div className="flex w-1/2 flex-col items-center">
             <h3 className="font-semibold">Views</h3>
-            <p className="text-xl font-semibold">{post.viewCount}</p>
+            <CountingNumbers
+              value={post.viewCount}
+              className="text-xl font-semibold"
+            />
           </div>
           <div className="flex w-1/2 flex-col items-center">
             <h3 className="font-semibold">Favourites</h3>
-            <p className="text-xl font-semibold" id="postFavouriteCount">
-              {post.favouriteCount}
-            </p>
+            <CountingNumbers
+              value={post.favouriteCount}
+              className="text-xl font-semibold"
+            />
           </div>
         </div>
       </CardContent>
