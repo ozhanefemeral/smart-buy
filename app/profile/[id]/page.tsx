6429538751ca18d7947f631c30c9f323aed0f832
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-03-11 22:59:57 +0100

    refactor: move data from layout to page

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,6 +1,12 @@
 import { PostOwnerInfo } from "@/components/post-owner";
 import { User } from "@prisma/client";
 import { Suspense } from "react";
+import { Notifications } from "./Notifications";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { getUserWithPosts } from "@/lib/queries/user";
+import { redirect } from "next/navigation";
+import { UserPosts } from "./UserPosts/UserPosts";
 
 export default async function ProfilePage({
   params,
@@ -9,11 +15,23 @@ export default async function ProfilePage({
 }) {
   const { id } = params;
 
+  const session = await getServerSession(authOptions);
+  const user = await getUserWithPosts(id, session);
+  const isSelf = session?.user?.email === user?.email;
+
+  if (!user) {
+    redirect("/404");
+  }
+
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <PostOwnerInfo id={id} />
-      </Suspense>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="grid h-fit grid-cols-1 gap-4 md:grid-cols-3">
+        <div className="col-span-1">
+          {isSelf && <Notifications user={user} />}
+          {!isSelf && <PostOwnerInfo id={id} />}
+        </div>
+        <div className="col-span-2"></div>
+      </div>
+    </Suspense>
   );
 }
