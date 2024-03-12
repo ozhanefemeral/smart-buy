e43e5747540feb329c3cc395d8d229d24128890d
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-03-12 15:58:21 +0100

    refactor: add favourite button

--- components/post/PostCard/PostCard.tsx
+++ components/post/PostCard/PostCard.tsx
@@ -5,6 +5,7 @@ import placeHolderSVG from "@/public/placeholder.svg";
 import { Post } from "@prisma/client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FavoritePostButton } from "../FavoritePostButton";
 interface PostCardProps {
   post: Post;
   /**
@@ -27,19 +28,25 @@ export const PostCard: React.FC<PostCardProps> = ({ post, width }) => {
       }`}
       onClick={handleClick}
     >
-      <CardHeader>
-        <Image
-          src={post.thumbnail ? getImageUrl(post.thumbnail) : placeHolderSVG}
-          alt={post.title}
-          width={350}
-          height={200}
-          className="aspect-3/2 rounded-md object-cover"
-        />
+      <CardHeader className="p-4">
+        <div className="-mx-4 -mt-4">
+          <Image
+            src={post.thumbnail ? getImageUrl(post.thumbnail) : placeHolderSVG}
+            alt={post.title}
+            width={350}
+            height={200}
+            className="aspect-3/2 rounded-t-xl object-cover"
+          />
+        </div>
+
         <CardTitle className="pt-2">{post.title}</CardTitle>
       </CardHeader>
-      <CardFooter className="mt-auto flex flex-col items-start justify-between">
-        <p className="font-bold">{post.price} zł</p>
-        <p className="text-xs">{formatPostDate(post.createdAt)}</p>
+      <CardFooter className="mt-auto flex justify-between p-4">
+        <div className="flex flex-col">
+          <p className="font-bold">{post.price} zł</p>
+          <p className="text-xs">{formatPostDate(post.createdAt)}</p>
+        </div>
+        <FavoritePostButton postId={post.id} />
       </CardFooter>
     </Card>
   );
