77569beaa2418b086c6ca4f1ca1ce37291fb9ce0
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-03-12 01:15:57 +0100

    refactor: rework card style
    - remove static width
    - remove descriptions
    - update imports

--- components/post/PostCard/PostCard.tsx
+++ components/post/PostCard/PostCard.tsx
@@ -1,16 +1,9 @@
 "use client";
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { formatPostDate, getImageUrl, timeAgo } from "@/lib/utils";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { formatPostDate, getImageUrl } from "@/lib/utils";
 import placeHolderSVG from "@/public/placeholder.svg";
 import { Post } from "@prisma/client";
 import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 interface PostCardProps {
   post: Post;
@@ -24,10 +17,7 @@ export const PostCard: React.FC<PostCardProps> = ({ post }) => {
   };
 
   return (
-    <Card
-      className="flex w-[300px] flex-col hover:cursor-pointer"
-      onClick={handleClick}
-    >
+    <Card className="flex flex-col hover:cursor-pointer" onClick={handleClick}>
       <CardHeader>
         <Image
           src={post.thumbnail ? getImageUrl(post.thumbnail) : placeHolderSVG}
@@ -37,7 +27,6 @@ export const PostCard: React.FC<PostCardProps> = ({ post }) => {
           className="aspect-3/2 rounded-md object-cover"
         />
         <CardTitle className="pt-2">{post.title}</CardTitle>
-        <CardDescription>{post.description}</CardDescription>
       </CardHeader>
       <CardFooter className="mt-auto flex flex-col items-start justify-between">
         <p className="font-bold">{post.price} zł</p>
